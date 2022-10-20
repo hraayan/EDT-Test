9eddba242392c2e667d8b27340b58ab4c42b96e2
hraayan <hraayan@gmail.com>
2022-10-20 01:32:15 -0500

    Updates

---
+++ precache-manifest.84c2a41682418942f9f4820de0fcf896.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "e76145ee235483ae5eab",
+    "url": "/EDT-Test/static/css/main.6999ace3.chunk.css"
+  },
+  {
+    "revision": "e76145ee235483ae5eab",
+    "url": "/EDT-Test/static/js/main.e76145ee.chunk.js"
+  },
+  {
+    "revision": "fbee9149cc341e4ff9a0",
+    "url": "/EDT-Test/static/js/1.fbee9149.chunk.js"
+  },
+  {
+    "revision": "6135f6271e827c980761",
+    "url": "/EDT-Test/static/js/2.6135f627.chunk.js"
+  },
+  {
+    "revision": "676c7ea2efad65841b29",
+    "url": "/EDT-Test/static/js/runtime~main.676c7ea2.js"
+  },
+  {
+    "revision": "0a78d80169c1142472aa1d3d54e18609",
+    "url": "/EDT-Test/static/media/restimg.0a78d801.jpg"
+  },
+  {
+    "revision": "a2da7237ca6c0bd730d5a6c9397eddfa",
+    "url": "/EDT-Test/static/media/food-wall.a2da7237.jpg"
+  },
+  {
+    "revision": "c19ebd6e70e8dbc0eb7cc911928a7e79",
+    "url": "/EDT-Test/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/EDT-FrontTest/precache-manifest.977eaba4323cab73129e0ca5576d1c7e.js"
+  "/EDT-Test/precache-manifest.84c2a41682418942f9f4820de0fcf896.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/EDT-FrontTest/index.html", {
+workbox.routing.registerNavigationRoute("/EDT-Test/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
